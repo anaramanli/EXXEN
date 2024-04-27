752717c3c93b5a72b17d83389a4e2291e78addae
anaramanli <amanlianar@gmail.com>
2024-04-28 00:46:30 +0400

    update-2

--- assset/js/main.js
+++ assset/js/main.js
@@ -3,30 +3,146 @@ fetch('https://api.tvmaze.com/shows')
     .then(data => {
         getMovies(data);
     });
-
 var area = document.getElementById("films");
-
 function getMovies(movies) {
     let html = '<div class="row">';
-    for (let i = 0; i < movies.length && i < 10; i++) {
+    for (let i = 0; i < movies.length && i < 11; i++) {
         html += `
             <div class="col-md-4">
                 <div class="film">
                     <img src="${movies[i].image.medium}" class="filmsimgtop" alt="${movies[i].name}"data-bs-toggle="modal" data-bs-target="#info" data-image ="${movies[i].image.original}">
+                    
                     <div class="movie-body">
                         <h5 class="movie-title">${movies[i].name}</h5>
                     </div>
+                    <a href="detail.html?id=${movies[i].id}" class="btn btn-primary"target=_blank>More Info</a>
                 </div>
             </div>`;
     }
     html += '</div>';
     area.innerHTML = html;
 
+    ///modal
     var filmsimgtops = document.querySelectorAll(".filmsimgtop");
     filmsimgtops.forEach(img => {
-        img.addEventListener("click", function() {
+        img.addEventListener("click", function () {
             var modalImage = document.getElementById("modalImage");
-                modalImage.src = this.getAttribute("data-image");
+            modalImage.src = this.getAttribute("data-image");
         });
     });
 }
+
+var dropdownItems = document.querySelectorAll(".dropdown-item");
+dropdownItems.forEach(item => {
+    item.addEventListener("click", function () {
+        if (this.id === "a-z") {
+            sortAlphabetically(this.textContent);
+        } else if (this.id === "z-a") {
+            sortReverseAlphabetically(this.textContent);
+        }
+    });
+});
+
+function sortAlphabetically(sortBy) {
+    alert("Sorting by: " + sortBy);
+    fetch('https://api.tvmaze.com/shows')
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => {
+                const nameA = a.name.toLowerCase();
+                const nameB = b.name.toLowerCase();
+                if (nameA < nameB) {
+                    return -1;
+                }
+                
+                return 0;
+            });
+            getMovies(data);
+    });
+}
+function sortReverseAlphabetically(sortBy) {
+    alert("Sorting by: " + sortBy);
+    fetch('https://api.tvmaze.com/shows')
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => {
+                const nameA = a.name.toLowerCase();
+                const nameB = b.name.toLowerCase();
+                if (nameA > nameB) {
+                    return -1;
+                }
+                if (nameA < nameB) {
+                    return 1;
+                }
+                return 0;
+            });
+            getMovies(data);
+    });
+}
+
+function sortMovies(sortBy) {
+    alert("Sorting by: " + sortBy);
+    fetch('https://api.tvmaze.com/shows')
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => b.rating.average - a.rating.average);
+            getMovies(data);
+        });
+}
+
+function sortReverse(sortBy) {
+    alert("Sorting by: " + sortBy);
+    fetch('https://api.tvmaze.com/shows')
+        .then(res => res.json())
+        .then(data => {
+            data.sort((a, b) => a.rating.average - b.rating.average);
+            getMovies(data);
+        });
+}
+
+// var searchInput = document.getElementById("search");
+// searchInput.addEventListener("keyup");
+// function search(event) {
+//     axios.get('https://api.tvmaze.com/search/shows?q=' + )
+//         .then(elements => fillHtml(elements.data))
+//     getMoviesSearch(data);
+// }
+/////////////////
+// var searchInput = document.getElementById("search");
+
+// function searchingMovie(e) {
+//     var query = searchInput.value.trim(); // Arama sorgusunu giriş alanından al
+//     axios.get(`https://api.tvmaze.com/search/shows?q=${query}`)
+//         .then(elements => fillHtml(elements.data))
+//         getMovies();
+//         .catch(error => console.error('Hata:', error));
+// }
+
+// // Arama giriş alanındaki keyup etkinliği için olay dinleyicisi
+// searchInput.addEventListener("keyup", searchingMovie);
+
+// searchInput.addEventListener()
+// function getMoviesSearch(movies) {
+//     let html = '<div class="row">';
+//     for (let i = 0; i < movies.length && i < 11; i++) {
+//         html += `
+//             <div class="col-md-4">
+//                 <div class="film">
+//                     <img src="${movies[i].image.medium}" class="filmsimgtop" alt="${movies[i].name}"data-bs-toggle="modal" data-bs-target="#info" data-image ="${movies[i].image.original}">
+//                     <div class="movie-body">
+//                         <h5 class="movie-title">${movies[i].name}</h5>
+//                     </div>
+//                 </div>
+//             </div>`;
+//     }
+//     html += '</div>';
+//     area.innerHTML = html;
+
+//     var filmsimgtops = document.querySelectorAll(".filmsimgtop");
+//     filmsimgtops.forEach(img => {
+//         img.addEventListener("click", function () {
+//             var modalImage = document.getElementById("modalImage");
+//             modalImage.src = this.getAttribute("data-image");
+//         });
+//     });
+// }
